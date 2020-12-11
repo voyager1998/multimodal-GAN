1ac8e561141c2e87a5e37bae0c1ffcb96533caf5
Zhihao Ruan <ruanzh@seas.upenn.edu>
2020-12-10 22:15:56 -0500

    add optional dataset downloading in train shell

--- scripts/download_and_train.sh
+++ scripts/download_and_train.sh
@@ -1,4 +1,11 @@
-cd data
-sh download_dataset.sh edges2shoes
-cd ..
+if [ -d "data/edges2shoes" ] 
+    then
+    echo "Dataset exists. No need to download dataset."
+else
+    echo "Downloading dataset. This may take a while..."
+    cd data
+    sh download_dataset.sh edges2shoes
+    cd ..
+fi
+
 python src/train.py
